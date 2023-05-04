1a3b018a002a4fa8753e706638a5866aa7388399
manlycracker65 <103338779+manlycracker65@users.noreply.github.com>
2023-05-03 19:26:03 -0500

    File
    Probably won't work for anyone but me

---
+++ config.js
@@ -0,0 +1,6 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://super-tooth-25b0.s08mna-eyeyxy5wpkdok4621.workers.dev/",
+  VITE_TMDB_API_KEY: "b030404650f279792a8d3287232358e3",
+  VITE_OMDB_API_KEY: "aa0937c0",
+};
